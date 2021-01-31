db4f36d1adadf0d7988c64309c18c91d4e3f34dd
Rodrigo MontAlegre <rodrigo.montalegre@hotmail.com>
2021-01-31 12:39:25 +0100

    V-Dem dataset now split into democracy and autocracies.

--- script.r
+++ script.r
@@ -4,40 +4,39 @@
 library(data.table)
 library(ggplot2)
 
-
-
 if (Sys.info()["user"] == "Rodrigo") {
   setwd("C:/Users/Rodrigo/Desktop/TUM/Wintersemester 2021/POL60700 Modul Democracy in the digital age/Paper/data")
 }
 
 cn <- fread("coronanet_release.csv")
 
-vdem <- fread("V-Dem-CY-Core-v10.csv")
+vdem <- fread("V-Dem-CY-Full+Others-v10.csv",
+              sep = ",",
+              encoding = "UTF-8")
+
 
 vdem_2019 <- vdem[year == "2019", .(country_name,
                                     country_text_id,
                                     year,
                                     histname,
-                                    v2elmulpar_osp,
-                                    v2elfrfair_osp,
-                                    v2x_polyarchy,
-                                    v2cltrnslw_osp, #transparent law enforcement
-                                    v2clacjstm_osp,
-                                    v2clacjstw_osp,
-                                    v2x_liberal
+                                    v2x_regime
                                     )]
 
 vdem_2019$regime_type <- NA
 
 for(i in 1:nrow(vdem_2019)) {
-  if(vdem_2019[i, 5] > 2 
-     && vdem_2019[i, 6] > 2 
-     && vdem_2019[i, 7] > 5 
-     && vdem_2019[i, 8] > 3 
-     && vdem_2019[i, 9] > 3 
-     && vdem_2019[i, 9] > 3 
-     && vdem_2019[i, 10] > 0.8) {
+  if(vdem_2019[i, "v2x_regime"] == 3){
     vdem_2019$regime_type[i] <- "liberal_democracy"
+  }else if(vdem_2019[i, "v2x_regime"] == 2){
+    vdem_2019$regime_type[i] <- "electoral_democracy"
+  }else if(vdem_2019[i, "v2x_regime"] == 1){
+    vdem_2019$regime_type[i] <- "electoral_autocracy"
+  }else if(vdem_2019[i, "v2x_regime"] == 0){
+    vdem_2019$regime_type[i] <- "closed_autocracy"
   }
-  else if
+}
+
+vdem_2019_dem <- vdem_2019[vdem_2019$v2x_regime == 3 | vdem_2019$v2x_regime == 2] #all countries classified as a type of democracy
+
+vdem_2019_aut <- vdem_2019[vdem_2019$v2x_regime == 1 | vdem_2019$v2x_regime == 0] #all countries classified as a type of autocracy
 
